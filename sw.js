ac569a3be955a273f72ebb9033bc9f43d99db700
GymNexusSoftware <gymnexus.software@gmail.com>
2026-04-05 23:15:52 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,34 @@
-const CACHE_NAME = 'mario-v1';
+const CACHE_NAME = 'mario-v3';
 const ASSETS = [
   './',
   './index.html',
+  './manifest.json',
   './heroi.png',
   './monstro1.png',
   './plataforma.jpg'
 ];
 
+// Instalação e Cache dos ficheiros
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
+// Ativação e limpeza de caches antigos
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+});
+
+// Responde com cache se estiver offline
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
